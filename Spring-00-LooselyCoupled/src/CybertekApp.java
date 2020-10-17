7b32939747f8690cb55348017c96adcf8a0c1b04
dogan1612 <kubilaydogan@yahoo.com>
2020-10-17 11:48:31 -0400

    Commit

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,13 @@
+import service.FullTimeMentor;
+import service.MentorAccount;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        FullTimeMentor fulltime = new FullTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(fulltime);
+        mentor.manageAccount();
+
+    }
+}
